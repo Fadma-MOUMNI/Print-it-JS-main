6e163b4ec74b515746c1a21529ff42f36634c568
fadma MOUMNI <fadmamoumni0@gmail.com>
2024-02-13 02:00:46 +0100

    Dernières mises à jour (Fin de projet)

--- assets/script.js
+++ assets/script.js
@@ -33,20 +33,55 @@ arrowRight.addEventListener("mouseover", () => {
 
 });
 
+
+
+// les points
+const Dots = document.querySelector(".dots"); // récupération de la div ".dots"
+// Ajout automatiquement d'un point pour chaque diapositive
+for (let i = 0; i < slides.length; i++) {
+  const point = document.createElement("span"); // creation d'un span
+  Dots.appendChild(point); // ajout de point au div parent// Prends(point) et mets-le dans (Dots).
+  point.classList.add("dot"); //ajout de la class "dot"
+}
+
+// Récupération des points
+const LesPoint = Dots.querySelectorAll(".dot");// tout les element avec la class ".dot"
+let activeDot = 0;
+// Ajout de la classe "dot_selected" sur le premier point
+LesPoint[activeDot].classList.add("dot_selected");
+
+//mettre à jour visuellement les points indicateurs lorsque la diapositive change.
+function updateDote(prev, activeSlide) {
+  LesPoint[prev].classList.remove("dot_selected");
+  LesPoint[activeSlide].classList.add("dot_selected");
+}
+
+
+
 //les slides 
 let activeSlide = 0;// Initialise une variable pour suivre l'index de la diapositive actuellement active,
 // en commençant par la première diapositive (index 0).
 
+
+
+//mettre à jour l'image et le texte affichés en fonction de l'indice de slide active
+function updateSlide(activeSlide) {
+  //mettre à jour l'image en remplaçant la source de l'image par celle de la diapositive active dans le tableau slides.
+  image.setAttribute('src', './assets/images/slideshow/' + slides[activeSlide].image);
+  //mettre à jour le texte en remplaçant le contenu HTML(tagText) par le texte de la diapositive active dans le tableau slides. 
+  tagText.innerHTML = slides[activeSlide].tagLine;
+
+}
 //****slide suivant
 function nextSlide() {
-  // index de lelement precedent 
-  let prev = activeSlide;//active slide = salide actuelle //l'index de la diapositive actuelle
+  // index de l'element precedent 
+  let prev = activeSlide;//crée la variable 'prev' qui stocke l'index de la diapositive actuellement active avant de changer d'index.
   activeSlide++;// incrémente la valeur de activeSlide de 1
   console.log(activeSlide);
   if (activeSlide > slides.length - 1) {
     activeSlide = 0;
   }
-  updateSlide(activeSlide);
+  updateSlide(activeSlide);//mettre à jour l'image et le texte affichés en fonction de l'indice de la diapositive active.
   updateDote(prev, activeSlide);
 }
 
@@ -54,7 +89,7 @@ function nextSlide() {
 //  slide précédent
 function prevSlide() {
 
-  let prev = activeSlide;
+  let prev = activeSlide;//crée la variable 'prev' qui stocke l'index de la diapositive actuellement active avant de changer d'index.
   activeSlide--;// décrémentation de l'index pour passer à la diapositive précédente
   if (activeSlide < 0) {
     activeSlide = slides.length - 1;// revenir à la dernière diapositive si on est sur la première diapositive
@@ -67,33 +102,9 @@ function prevSlide() {
 }
 
 
-//mettre à jour l'image et le texte affichés en fonction de l'indice de slide active
-function updateSlide(activeSlide) {
-  image.setAttribute('src', './assets/images/slideshow/' + slides[activeSlide].image);
-  tagText.innerHTML = slides[activeSlide].tagLine;
-
-}
-
-// les points
-const Dots = document.querySelector(".dots"); // récupération de la div ".dots"
-// Ajout d'un point pour chaque diapositive
-for (let i = 0; i < slides.length; i++) {
-  const point = document.createElement("span"); // creation d'un span
-  Dots.appendChild(point); // ajout de point au div parent
-  point.classList.add("dot"); //ajout de la class "dot"
-}
-
-// Récupération des points
-const LesPoint = Dots.querySelectorAll(".dot");// tout les element avec la class ".dot"
-let activeDot = 0;
 
 
-// Ajout de la classe "dot_selected" sur le premier point
-LesPoint[activeDot].classList.add("dot_selected");
-function updateDote(prev, next) {
-  LesPoint[prev].classList.remove("dot_selected");
-  LesPoint[next].classList.add("dot_selected");
-}
+
 // le défilement automatique
 setInterval("nextSlide()", 5000);
 
